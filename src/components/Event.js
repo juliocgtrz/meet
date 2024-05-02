831a7139644de9d493528df96edc7ce90dea80b5
Julio G <juliocgtrz@gmail.com>
2024-05-01 23:50:52 -0600

    Update Event.js
    updated to include description and buttons

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,9 +1,26 @@
 // src/components/Event.js
 
-const Event = () => {
+import { useState } from "react";
+
+const Event = ({ event }) => {
+
+    const [showDetails, setShowDetails] = useState(false);
+
     return (
-        <li></li>
+        <li className="event">
+            <h2>{event.summary}</h2>
+            <p>{event.location}</p>
+            <p>{event.created}</p>
+            {showDetails ?
+                <p className="details">{event.description}</p>
+                : null
+            }
+            <button className="details-btn"
+            onClick={() => { showDetails ? setShowDetails(false) : setShowDetails(true) }}>
+                {showDetails ? 'Hide Details' : 'Show Details'}
+            </button>
+        </li>
     );
-}
+};
 
 export default Event;
