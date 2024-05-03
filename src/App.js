b9de49cd1b6dda3cecc00e14465f2a294ba5b185
Julio G <juliocgtrz@gmail.com>
2024-05-02 22:49:34 -0600

    Update App.js
    added code to retrieve list of events

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import { useEffect, useState } from 'react';
+import { getEvents } from './api';
 import './App.css';
 
+
 function App() {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <CitySearch />
-      <EventList />
+      <EventList events={events} />
       <NumberOfEvents />
     </div>
   );
