03a402e68179316e9cf6e474c3a33b10cfe8cc04
Julio G <juliocgtrz@gmail.com>
2024-05-03 22:55:00 -0600

    Update NumberOfEvents.test.js
    cleaned up code

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -7,19 +7,21 @@ import NumberOfEvents from '../components/NumberOfEvents';
 describe('<NumberOfEvents /> component', () => {
     let NumberOfEventsComponent;
     beforeEach(() => {
-        NumberOfEventsComponent = render(<NumberOfEvents setCurrentNOE={() => { }} setErrorAlert={() => { }} />);
+        NumberOfEventsComponent = render(<NumberOfEvents />);
     })
 
     test('contains element with role "textbox"', () => {
         const numberTextBox = NumberOfEventsComponent.queryByRole('textbox');
         expect(numberTextBox).toBeInTheDocument();
+        expect(numberTextBox).toHaveClass('number-of-events-input');
     });
-    test('32 events are rendered by default', () => {
-        expect(NumberOfEventsComponent.queryByRole('textbox')).toHaveValue('32');
+    test('32 events are rendered by default', async () => {
+        const numberTextBox = NumberOfEventsComponent.queryByRole('textbox');
+        expect(numberTextBox).toHaveValue('32');
     });
     test('value of number of events updates correctly when user types in textbox', async () => {
-        const numberOfEvents = NumberOfEventsComponent.queryByRole('textbox');
         const user = userEvent.setup();
+        const numberOfEvents = NumberOfEventsComponent.queryByRole('textbox');
         await user.type(numberOfEvents, '{backspace}{backspace}10');
         expect(numberOfEvents).toHaveValue('10');
     });
