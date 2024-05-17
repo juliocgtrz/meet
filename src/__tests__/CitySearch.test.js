6a7da762cc95d56206a179d7de1a73fc63cf7263
Julio G <juliocgtrz@gmail.com>
2024-05-16 22:16:38 -0600

    Update CitySearch.test.js
    added infoAlert

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -9,7 +9,11 @@ import App from '../App';
 describe('<CitySearch /> component', () => {
     let CitySearchComponent;
     beforeEach(() => {
-        CitySearchComponent = render(<CitySearch allLocations={[]}/>);
+        CitySearchComponent = render(<CitySearch
+            allLocations={[]}
+            setCurrentCity={() => { }}
+            setInfoAlert={() => { }}
+        />);
     });
     test('renders text input', () => {
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -32,7 +36,10 @@ describe('<CitySearch /> component', () => {
         const user = userEvent.setup();
         const allEvents = await getEvents();
         const allLocations = extractLocations(allEvents);
-        CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+        CitySearchComponent.rerender(<CitySearch
+            allLocations={allLocations}
+            setInfoAlert={() => { }}
+        />);
 
         // user types "Berlin" in city textbox
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -57,6 +64,7 @@ describe('<CitySearch /> component', () => {
         CitySearchComponent.rerender(<CitySearch
             allLocations={allLocations}
             setCurrentCity={() => { }}
+            setInfoAlert={() => { }}
         />);
 
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
