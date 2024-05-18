c16d2245f711610505a96ef73563720729643200
Julio G <juliocgtrz@gmail.com>
2024-05-18 13:40:07 -0600

    Update App.js
    corrected the variable

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const App = () => {
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
-        {errorAlert.length ? <WarningAlert text={warningAlert}/> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
       </div>
       <CitySearch
         allLocations={allLocations}
